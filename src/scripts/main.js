a018b9ad224db89e38883d547428c5c0d03eb39f
Miroslava <777shadowpriest777@gmail.com>
2019-11-02 15:48:14 +0200

    адаптивчик мобилка

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,4 +4,9 @@ $(document).ready(function () {
     dots: true,
     slidesToShow: 1
   });
+
+  $('#nav-icon').on('click', function(){
+    $(this).toggleClass('open');
+    $('.mob-nav__menu').toggleClass('show');
+  });
 });
